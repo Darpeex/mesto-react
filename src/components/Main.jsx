6783bb70cb781261dbd7616dacf2f35adc2d9686
darpeex <foxyyysp@mail.ru>
2023-06-26 19:02:41 +0300

    feat: добавлены императивные обработчики событий

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,19 @@
 import logo from '../images/avatarBlack.jpg';
 
 export function Main() {
+  const handleEditAvatarClick = () => {
+    const popup = document.querySelector('#updateAvatar');
+    popup.classList.add('popup_opened');
+  }
+  const handleEditProfileClick = () => {
+    const popup = document.querySelector('#updateAvatar');
+    popup.classList.add('popup_opened');
+  }
+  const handleAddPlaceClick = () => {
+    const popup = document.querySelector('#updateAvatar');
+    popup.classList.add('popup_opened');
+  }
+
   return (
     <main className="content">
 
@@ -9,15 +22,15 @@ export function Main() {
         <div className="profile__info">
           <div className="profile__avatar">
             <img className="profile__avatar-image" src={logo} alt='Аватарка'/>
-            <button className="profile__avatar-button" type="button" aria-label="Обновить аватарку"></button>
+            <button className="profile__avatar-button" type="button" onClick={handleEditAvatarClick} aria-label="Обновить аватарку"></button>
           </div>
           <div className="profile__content">
             <h1 className="profile__name">Человек</h1>
             <p className="profile__activity">Исследователь мира</p>
-            <button className="profile__button profile__button_action_edit" type="button" aria-label="Редактировать"></button>
+            <button className="profile__button profile__button_action_edit" type="button" onClick={handleEditProfileClick} aria-label="Редактировать"></button>
           </div>
         </div>
-        <button className="profile__button profile__button_action_add" type="button" aria-label="Добавить"></button>
+        <button className="profile__button profile__button_action_add" type="button" onClick={handleAddPlaceClick} aria-label="Добавить"></button>
       </section>
 
 {/* <!-- Секция, блок elements --> */}
